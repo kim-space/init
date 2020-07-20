8fa6282efbcdc922e70264290c90059c5791e1f7
mritd <mritd@linux.com>
2020-07-20 21:15:56 +0800

    feat(ohmyzsh): add github proxy support
    add github proxy support
    
    Signed-off-by: mritd <mritd@linux.com>

--- ohmyzsh/init.sh
+++ ohmyzsh/init.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-OHMYZSH_URL='https://github.com/robbyrussell/oh-my-zsh.git'
-OHMYZSH_CONFIG_URL='https://raw.githubusercontent.com/mritd/init/master/ohmyzsh/zshrc'
-SYNTAX_HIGHLIGHTING_URL='https://github.com/zsh-users/zsh-syntax-highlighting.git'
+GITHUB_HOST=${GITHUB_HOST-"github.com"}
+GITHUB_RAW_HOST=${GITHUB_RAW_HOST-"raw.githubusercontent.com"}`
+OHMYZSH_URL="https://${GITHUB_HOST}/robbyrussell/oh-my-zsh.git"
+OHMYZSH_CONFIG_URL="https://${GITHUB_RAW_HOST}/mritd/init/master/ohmyzsh/zshrc"
+SYNTAX_HIGHLIGHTING_URL="https://${GITHUB_HOST}/zsh-users/zsh-syntax-highlighting.git
 
 backup_timestamp=$(date "+%Y%m%d%H%M%S")
 
