3b97e406e0159f9d9e6ed04c87041a82ec1941b7
mritd <mritd@linux.com>
2020-07-20 18:46:42 +0800

    feat(ohmyzsh): add ohmyzsh
    add ohmyzsh
    
    Signed-off-by: mritd <mritd@linux.com>

---
+++ ohmyzsh/init.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e
+
+OHMYZSH_URL='https://github.com/robbyrussell/oh-my-zsh.git'
+OHMYZSH_CONFIG_URL='https://raw.githubusercontent.com/mritd/init/master/ohmyzsh/zshrc'
+SYNTAX_HIGHLIGHTING_URL='https://github.com/zsh-users/zsh-syntax-highlighting.git'
+
+backup_timestamp=$(date "+%Y%m%d%H%M%S")
+
+function backup_config(){
+    info "backup old config..."
+    if [ -f ~/.zshrc ]; then
+        warn "backup [~/.zshrc] to [~/.zshrc-${backup_timestamp}]"
+        mv ~/.zshrc ~/.zshrc-${backup_timestamp}
+    fi
+    if [ -d ~/.oh-my-zsh ]; then
+        warn "backup [~/.oh-my-zsh] to [~/.oh-my-zsh-${backup_timestamp}]"
+        mv ~/.oh-my-zsh ~/.oh-my-zsh-${backup_timestamp}
+    fi
+}
+
+function install_ohmyzsh(){
+    info "install zshrc..."
+    curl -sSL ${OHMYZSH_CONFIG_URL} > ~/.zshrc
+    info "install ohmyzsh..."
+    git clone --depth=1 ${OHMYZSH_URL} ~/.oh-my-zsh
+    info "change default shell to zsh..."
+    chsh -s $(grep /zsh$ /etc/shells | tail -1)
+}
+
+function install_syntax_highlighting(){
+    info "install zsh-syntax-highlighting..."
+    git clone ${SYNTAX_HIGHLIGHTING_URL} ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+}
+
+function info(){
+    echo -e "\033[32mINFO: $@\033[0m"
+}
+
+function warn(){
+    echo -e "\033[33mWARN: $@\033[0m"
+}
+
