603fce211ea63a5703a8e9636743fdb99d596d55
mritd <mritd@linux.com>
2020-07-20 18:28:40 +0800

    feat(vim): add sh opt
    add sh opt
    
    Signed-off-by: mritd <mritd@linux.com>

--- vim/init.sh
+++ vim/init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 ENABLE_VIM_CONFIG="true"
 VIM_CONFIG_URL="https://raw.githubusercontent.com/mritd/init/master/vim/vimrc"
 VIM_PLUG_URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
