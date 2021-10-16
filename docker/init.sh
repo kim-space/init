5cf0cf3c44408b0268b0a8b674f0c21b7cc29a1d
mritd <mritd@linux.com>
2021-10-16 23:34:14 +0800

    feat(docker): fix compose install
    fix compose install
    
    Signed-off-by: mritd <mritd@linux.com>

--- docker/init.sh
+++ docker/init.sh
@@ -3,33 +3,17 @@
 set -e
 
 OS_RELEASE="$(lsb_release -cs)"
-GITHUB_HOST=${GITHUB_HOST-"github.com"}
-GITHUB_RAW_HOST=${GITHUB_RAW_HOST-"raw.githubusercontent.com"}
 USE_APT_MIRROR=${USE_APT_MIRROR-"true"}
-DOCKER_APT_LIST_URL="https://${GITHUB_RAW_HOST}/mritd/init/master/docker/docker.list"
-DOCKER_SYSTEMD_CONFIG_URL="https://${GITHUB_RAW_HOST}/mritd/init/master/docker/docker.service"
-DOCKER_COMPOSE_URL="https://${GITHUB_HOST}/docker/compose/releases/download/1.26.2/docker-compose-Linux-x86_64"
+DOCKER_APT_LIST_URL="https://raw.githubusercontent.com/mritd/init/master/docker/docker.list"
+DOCKER_SYSTEMD_CONFIG_URL="https://raw.githubusercontent.com/mritd/init/master/docker/docker.service"
+DOCKER_COMPOSE_URL="https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m)"
 
 backup_timestamp=$(date "+%Y%m%d%H%M%S")
 
 function install_docker(){
     info "install docker..."
-    if [ "${OS_RELEASE}" == "focal" ]; then
-        apt install docker.io -y
-        apt-mark hold docker docker.io containerd
-    else
-        if [ "${USE_APT_MIRROR}" == "true" ]; then
-            info "use aliyun docker apt mirror..."
-            curl -sSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | apt-key add -
-            curl -sSL ${DOCKER_APT_LIST_URL} | sed "s@{{OS_RELEASE}}@${OS_RELEASE}@gi" > /etc/apt/sources.list.d/docker.list
-        else
-            curl -sSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-            echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu ${OS_RELEASE} stable" > /etc/apt/sources.list.d/docker.list
-        fi
-        apt update -y
-        apt install docker-ce -y
-        apt-mark hold docker docker-ce containerd
-    fi
+    apt install docker.io -y
+    apt-mark hold docker docker.io containerd
 
     curl -sSL ${DOCKER_SYSTEMD_CONFIG_URL} > docker.service
     SYSTEMD_EDITOR="mv docker.service" systemctl edit docker
@@ -38,8 +22,10 @@ function install_docker(){
 
 function install_compose(){
     info "install docker-compose..."
-    curl -sSL ${DOCKER_COMPOSE_URL} > /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
+
+    mkdir -p /usr/local/lib/docker/cli-plugins/
+    curl -sSL ${DOCKER_COMPOSE_URL} > /usr/local/lib/docker/cli-plugins/docker-compose
+    chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 }
 
 function info(){
