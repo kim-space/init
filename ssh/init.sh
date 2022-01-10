e0a6bf9dccdd1d05be8774d8d9e23e46461fd760
mritd <mritd@linux.com>
2022-01-10 10:48:37 +0800

    feat(ssh): add ssh init
    add ssh init
    
    Signed-off-by: mritd <mritd@linux.com>

---
+++ ssh/init.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+function init_ssh(){
+    info "Enable root login..."
+    cat > /etc/ssh/sshd_config.d/10-sshd-init.conf <<EOF
+PermitRootLogin yes
+AcceptEnv ENABLE_VIM_CONFIG MMH*
+EOF
+}
+
+function info(){
+    echo -e "\033[32mINFO: $@\033[0m"
+}
+
+function warn(){
+    echo -e "\033[33mWARN: $@\033[0m"
+}
+
+function err(){
+    echo -e "\033[31mERROR: $@\033[0m"
+}
